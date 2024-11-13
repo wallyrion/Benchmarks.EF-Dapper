eac01ae8854fb50dd7f796b5132509c92fd6eeeb
Oleksii KorniienKo <kornienko1296@gmail.com>
2024-11-13 14:15:47 +0100

    Change k6 script to run with 256m of memory

--- k6-scripts/k6.js
+++ k6-scripts/k6.js
@@ -4,7 +4,7 @@ import { randomIntBetween } from 'https://jslib.k6.io/k6-utils/1.2.0/index.js';
 export const options = {
     stages: [
         {duration: '10s', target: 5},
-        {duration: '5m', target: 5000},
+        {duration: '5m', target: 2000},
         {duration: '1m', target: 0}
     ],
     thresholds: {
